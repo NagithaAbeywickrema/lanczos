41f7855a99a433596558d97946308d42bfd81e57
Ravindu-Hirimuthugoda <ravindu.18@cse.mrt.ac.lk>
2023-05-25 13:59:14 +0530

    Add reduction

--- src/lanczos.h
+++ src/lanczos.h
@@ -8,4 +8,4 @@ void create_lap(double *lap, const int SIZE);
 void print_matrix(double *matrix, const int SIZE1, const int SIZE2);
 void print_eigen_vals(double *eigen_vals, const int SIZE);
 void lanczos(double *lap, const int SIZE, const int M, double *eigvals,
-             double *eigvecs);
+             double *eigvecs, int argc, char *argv[]);
--- src/mtx.h
+++ src/mtx.h
@@ -1,8 +1,8 @@
-double mtx_norm(double *w, double *d_w, const unsigned SIZE);
-void mtx_sclr_div(double *v, double *w, double sclr, const unsigned SIZE);
-void mtx_col_copy(double *v, double *V, int i, unsigned SIZE);
-void mtx_mul(double *a, double *b, double *out, const int h_a, const int w_a,
-             const int w_b);
-double mtx_dot(double *v, double *w, double *d_w, const int SIZE);
-void mtx_tri(double *alpha, double *beta, double *T, const int M);
-void mtx_identity(double *out, const int SIZE);
+void nomp_mtx_norm(double *w, double *prod, const unsigned SIZE);
+void nomp_mtx_sclr_div(double *v, double *w, double sclr, const unsigned SIZE);
+void nomp_mtx_col_copy(double *v, double *V, int i, unsigned SIZE);
+void nomp_mtx_mul(double *a, double *b, double *out, const int h_a,
+                  const int w_a, const int w_b);
+void nomp_mtx_dot(double *v, double *w, double *prod, const unsigned SIZE);
+void nomp_mtx_tri(double *alpha, double *beta, double *T, const int M);
+void nomp_mtx_identity(double *out, const int SIZE);
