e213a2d7150429058aeeb256aef10e53222918f4
pubuduprabhathiya <pubuduprabhathiya@gmail.com>
2023-05-25 15:49:46 +0530

    Add mtx_norm SYCL code

---
+++ src/sycl/lanczos.h
@@ -0,0 +1,10 @@
+#include <CL/sycl.hpp>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void create_lap(double *lap, const int SIZE);
+void lanczos(double *lap, const int SIZE, const int M, double *eigvals,
+             double *eigvecs, sycl::queue queue);
