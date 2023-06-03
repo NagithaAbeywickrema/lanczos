5a8b7a17edeb35cb7c4ba240f816bdc479446f23
NagithaAbeywickrema <nagitha98@gmail.com>
2023-06-03 18:55:08 +0530

    Refine serial code

--- src/kernels.h
+++ src/kernels.h
@@ -1,14 +1,17 @@
 // Serially implemented functions of Lanczos routine.
-double vec_norm(double *w, const unsigned size);
-void mtx_sclr_div(double *v, double *w, double sclr, const unsigned size);
-void mtx_col_copy(double *v, double *V, int i, unsigned size);
-void mtx_vec_mul(double *a, double *b, double *out, const int h_a,
-                 const int w_a);
-double vec_dot(double *v, double *w, const unsigned size);
-void calc_w_init(double *w, double alpha, double *V, unsigned i,
-                 const int size);
-void calc_w(double *w, double alpha, double *V, double beta, unsigned i,
-            const int size);
+double serial_vec_dot(double *a_vec, double *b_vec, const unsigned size);
+double serial_vec_norm(double *a_vec, const unsigned size);
+void serial_vec_sclr_div(double *a_vec, double *out_vec, const double sclr,
+                         const unsigned size);
+void serial_mtx_col_copy(double *vec, double *mtx, const unsigned col_index,
+                         const unsigned size);
+void serial_mtx_vec_mul(double *a_mtx, double *b_vec, double *out_vec,
+                        const unsigned num_rows, const unsigned num_cols);
+void serial_calc_w_init(double *w_vec, const double alpha, double *orth_mtx,
+                        const unsigned col_index, const unsigned size);
+void serial_calc_w(double *w_vec, const double alpha, double *orth_mtx,
+                   const double beta, const unsigned col_index,
+                   const unsigned size);
 
 // Parallelized functions of Lanczos routine implemented using Nomp.
 double nomp_vec_norm(double *w, const unsigned size);
--- src/lanczos-serial.c
+++ src/lanczos-serial.c
@@ -6,30 +6,31 @@
 #define MAX 1000000
 #define EPS 1e-12
 
-void lanczos_algo(double *lap, double *alpha, double *beta, double *w,
-                  double *v, double *V, const unsigned m, const unsigned size) {
+void lanczos_algo(double *lap, double *alpha, double *beta, double *w_vec,
+                  double *orth_vec, double *orth_mtx, const unsigned m,
+                  const unsigned size) {
   for (unsigned i = 0; i < m; i++) {
-    beta[i] = vec_norm(w, size);
+    beta[i] = serial_vec_norm(w_vec, size);
 
     if (fabs(beta[i] - 0) > 1e-8) {
-      mtx_sclr_div(w, v, beta[i], size);
+      serial_vec_sclr_div(w_vec, orth_vec, beta[i], size);
     } else {
       for (unsigned i = 0; i < size; i++)
-        v[i] = (double)rand() / (double)(RAND_MAX / MAX);
-      double norm_val = vec_norm(v, size);
-      mtx_sclr_div(v, v, norm_val, size);
+        orth_vec[i] = (double)rand() / (double)(RAND_MAX / MAX);
+      double norm_val = serial_vec_norm(orth_vec, size);
+      serial_vec_sclr_div(orth_vec, orth_vec, norm_val, size);
     }
 
-    mtx_col_copy(v, V, i, size);
+    serial_mtx_col_copy(orth_vec, orth_mtx, i, size);
 
-    mtx_vec_mul(lap, v, w, size, size);
+    serial_mtx_vec_mul(lap, orth_vec, w_vec, size, size);
 
-    alpha[i] = vec_dot(v, w, size);
+    alpha[i] = serial_vec_dot(orth_vec, w_vec, size);
 
     if (i == 0) {
-      calc_w_init(w, alpha[i], V, i, size);
+      serial_calc_w_init(w_vec, alpha[i], orth_mtx, i, size);
     } else {
-      calc_w(w, alpha[i], V, beta[i], i, size);
+      serial_calc_w(w_vec, alpha[i], orth_mtx, beta[i], i, size);
     }
   }
 }
@@ -39,17 +40,17 @@ void lanczos(double *lap, const unsigned size, const unsigned m,
   // print_matrix(lap, size, size);
 
   // Allocate memory
-  double *V = (double *)calloc(size * m, sizeof(double));
+  double *orth_mtx = (double *)calloc(size * m, sizeof(double));
   double *alpha = (double *)calloc(m, sizeof(double));
   double *beta = (double *)calloc(m, sizeof(double));
-  double *v = (double *)calloc(size, sizeof(double));
-  double *w = (double *)calloc(size, sizeof(double));
+  double *orth_vec = (double *)calloc(size, sizeof(double));
+  double *w_vec = (double *)calloc(size, sizeof(double));
 
   for (int k = 0; k < 10; k++)
-    lanczos_algo(lap, alpha, beta, w, v, V, m, size);
+    lanczos_algo(lap, alpha, beta, w_vec, orth_vec, orth_mtx, m, size);
 
   clock_t t = clock();
-  lanczos_algo(lap, alpha, beta, w, v, V, m, size);
+  lanczos_algo(lap, alpha, beta, w_vec, orth_vec, orth_mtx, m, size);
   t = clock() - t;
   printf("size: %d, time: %e \n", size, (double)t / (CLOCKS_PER_SEC));
 
