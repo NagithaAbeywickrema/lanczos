4d4979522c184fd5af662cdd893a4e0e968c593e
NagithaAbeywickrema <nagitha98@gmail.com>
2023-06-03 18:55:08 +0530

    Refine serial code

--- src/kernels-serial.c
+++ src/kernels-serial.c
@@ -3,60 +3,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-void create_identity_matrix(double *out, const int size) {
-  for (unsigned i = 0; i < size; i++) {
-    for (unsigned j = 0; j < size; j++) {
-      if (i == j)
-        out[i * size + j] = 1;
-      else
-        out[i * size + j] = 0;
-    }
-  }
-}
-
-void mtx_vec_mul(double *A, double *B, double *out, const int height_a,
-                 const int width_a) {
-  for (unsigned i = 0; i < height_a; i++) {
-    double dot = 0;
-    for (unsigned k = 0; k < width_a; k++)
-      dot += A[i * width_a + k] * B[k];
-    out[i] = dot;
-  }
-}
-
-double vec_dot(double *v, double *w, const int size) {
+double serial_vec_dot(double *a_vec, double *b_vec, const unsigned size) {
   double dot = 0;
   for (unsigned i = 0; i < size; i++)
-    dot += v[i] * w[i];
+    dot += a_vec[i] * b_vec[i];
   return dot;
 }
-void mtx_sclr_div(double *in, double *out, double scalar, const int size) {
-  for (unsigned i = 0; i < size; i++)
-    out[i] = in[i] / scalar;
+
+double serial_vec_norm(double *a_vec, const unsigned size) {
+  double sum_of_prod = serial_vec_dot(a_vec, a_vec, size);
+  return sqrt(sum_of_prod);
 }
 
-void calc_w_init(double *w, double alpha, double *V, unsigned i,
-                 const int size) {
-  for (int j = 0; j < size; j++) {
-    w[j] = w[j] - alpha * V[j + size * i];
+void serial_vec_sclr_div(double *a_vec, double *out_vec, const double sclr,
+                         const unsigned size) {
+  for (unsigned i = 0; i < size; i++)
+    out_vec[i] = a_vec[i] / sclr;
+}
+
+void serial_mtx_col_copy(double *vec, double *mtx, const unsigned col_index,
+                         const unsigned size) {
+
+  memcpy(mtx + size * col_index, vec, sizeof(double) * size);
+}
+
+void serial_mtx_vec_mul(double *a_mtx, double *b_vec, double *out_vec,
+                        const unsigned num_rows, const unsigned num_cols) {
+  for (unsigned i = 0; i < num_rows; i++) {
+    double dot = 0;
+    for (unsigned k = 0; k < num_cols; k++)
+      dot += a_mtx[i * num_cols + k] * b_vec[k];
+    out_vec[i] = dot;
   }
 }
 
-void calc_w(double *w, double alpha, double *V, double beta, unsigned i,
-            const int size) {
-  for (int j = 0; j < size; j++) {
-    w[j] = w[j] - alpha * V[j + size * i] - beta * V[j + size * (i - 1)];
+void serial_calc_w_init(double *w_vec, const double alpha, double *orth_mtx,
+                        const unsigned col_index, const unsigned size) {
+  for (unsigned j = 0; j < size; j++) {
+    w_vec[j] = w_vec[j] - alpha * orth_mtx[j + size * col_index];
   }
 }
 
-double vec_norm(double *w, const int size) {
-  double total = 0;
-  for (unsigned i = 0; i < size; i++)
-    total += w[i] * w[i];
-  return sqrt(total);
-}
-
-void mtx_col_copy(double *v, double *V, int j, const int size) {
-
-  memcpy(V + size * j, v, sizeof(double) * size);
+void serial_calc_w(double *w_vec, const double alpha, double *orth_mtx,
+                   const double beta, const unsigned col_index,
+                   const unsigned size) {
+  for (unsigned j = 0; j < size; j++) {
+    w_vec[j] = w_vec[j] - alpha * orth_mtx[j + size * col_index] -
+               beta * orth_mtx[j + size * (col_index - 1)];
+  }
 }
