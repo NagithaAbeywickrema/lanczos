13568a1c0ea5020fc866c10b09de6572dd6b4e39
NagithaAbeywickrema <nagitha98@gmail.com>
2023-06-03 19:20:10 +0530

    Refine nomp implementation

--- src/kernels-nomp.c
+++ src/kernels-nomp.c
@@ -1,59 +1,63 @@
 #include <math.h>
 
-void nomp_mtx_sclr_div(double *v, double *w, double sclr, const unsigned size) {
-#pragma nomp for transform("transforms", "matrix_norm")
-  for (unsigned i = 0; i < size; i++)
-    w[i] = v[i] / sclr;
-#pragma nomp sync
-}
-
-void nomp_mtx_col_copy(double *v, double *V, int i, unsigned size) {
-#pragma nomp for transform("transforms", "matrix_norm")
-  for (unsigned j = 0; j < size; j++)
-    V[j + size * i] = v[j];
-#pragma nomp sync
-}
-
-void nomp_mtx_vec_mul(double *a, double *b, double *out, const int h_a,
-                      const int w_a) {
-#pragma nomp for transform("transforms", "matrix_mul")
-  for (unsigned i = 0; i < h_a; i++) {
-    double dot = 0;
-    for (unsigned k = 0; k < w_a; k++)
-      dot += a[i * w_a + k] * b[k];
-    out[i] = dot;
-  }
-#pragma nomp sync
-}
-
-double nomp_vec_dot(double *v, double *w, const unsigned size) {
+double nomp_vec_dot(double *a_vec, double *b_vec, const unsigned size) {
   double prod[1] = {0};
 #pragma nomp for reduce("prod", "+")
   for (unsigned i = 0; i < size; i++)
-    prod[0] += v[i] * w[i];
+    prod[0] += a_vec[i] * b_vec[i];
 #pragma nomp sync
   return prod[0];
 }
 
-double nomp_vec_norm(double *w, const unsigned size) {
-  double prod = nomp_vec_dot(w, w, size);
-  return sqrt(prod);
+double nomp_vec_norm(double *a_vec, const unsigned size) {
+  double sum_of_prod = nomp_vec_dot(a_vec, a_vec, size);
+  return sqrt(sum_of_prod);
 }
 
-void nomp_calc_w_int(double *w, double alpha, double *V, unsigned i,
-                     const int size) {
+void nomp_vec_sclr_div(double *a_vec, double *out_vec, const double sclr,
+                       const unsigned size) {
 #pragma nomp for transform("transforms", "matrix_norm")
-  for (int j = 0; j < size; j++) {
-    w[j] = w[j] - alpha * V[j + size * i];
+  for (unsigned i = 0; i < size; i++)
+    out_vec[i] = a_vec[i] / sclr;
+#pragma nomp sync
+}
+
+void nomp_mtx_col_copy(double *vec, double *mtx, const unsigned col_index,
+                       const unsigned size) {
+#pragma nomp for transform("transforms", "matrix_norm")
+  for (unsigned j = 0; j < size; j++)
+    mtx[j + size * col_index] = vec[j];
+#pragma nomp sync
+}
+
+void nomp_mtx_vec_mul(double *a_mtx, double *b_vec, double *out_vec,
+                      const unsigned num_rows, const unsigned num_cols) {
+#pragma nomp for transform("transforms", "matrix_mul")
+  for (unsigned i = 0; i < num_rows; i++) {
+    double dot = 0;
+    for (unsigned k = 0; k < num_cols; k++)
+      dot += a_mtx[i * num_cols + k] * b_vec[k];
+    out_vec[i] = dot;
   }
 #pragma nomp sync
 }
 
-void nomp_calc_w(double *w, double alpha, double *V, double beta, unsigned i,
-                 const int size) {
+void nomp_calc_w_init(double *w_vec, const double alpha, double *orth_mtx,
+                      const unsigned col_index, const int size) {
 #pragma nomp for transform("transforms", "matrix_norm")
-  for (int j = 0; j < size; j++) {
-    w[j] = w[j] - alpha * V[j + size * i] - beta * V[j + size * (i - 1)];
+  for (unsigned j = 0; j < size; j++) {
+    w_vec[j] = w_vec[j] - alpha * orth_mtx[j + size * col_index];
+  }
+#pragma nomp sync
+}
+
+void nomp_calc_w(double *w_vec, const double alpha, double *orth_mtx,
+                 const double beta, const unsigned col_index,
+                 const unsigned size) {
+#pragma nomp for transform("transforms", "matrix_norm")
+  for (unsigned j = 0; j < size; j++) {
+    w_vec[j] = w_vec[j] - alpha * orth_mtx[j + size * col_index] -
+               beta * orth_mtx[j + size * (col_index - 1)];
   }
 #pragma nomp sync
 }
--- src/kernels.h
+++ src/kernels.h
@@ -14,16 +14,19 @@ void serial_calc_w(double *w_vec, const double alpha, double *orth_mtx,
                    const unsigned size);
 
 // Parallelized functions of Lanczos routine implemented using Nomp.
-double nomp_vec_norm(double *w, const unsigned size);
-void nomp_mtx_sclr_div(double *v, double *w, double sclr, const unsigned size);
-void nomp_mtx_col_copy(double *v, double *V, int i, unsigned size);
-void nomp_mtx_vec_mul(double *a, double *b, double *out, const int h_a,
-                      const int w_a);
-double nomp_vec_dot(double *v, double *w, const unsigned size);
-void nomp_calc_w_int(double *w, double alpha, double *V, unsigned i,
-                     const int size);
-void nomp_calc_w(double *w, double alpha, double *V, double beta, unsigned i,
-                 const int size);
+double nomp_vec_dot(double *a_vec, double *b_vec, const unsigned size);
+double nomp_vec_norm(double *a_vec, const unsigned size);
+void nomp_vec_sclr_div(double *a_vec, double *out_vec, const double sclr,
+                       const unsigned size);
+void nomp_mtx_col_copy(double *vec, double *mtx, const unsigned col_index,
+                       const unsigned size);
+void nomp_mtx_vec_mul(double *a_mtx, double *b_vec, double *out_vec,
+                      const unsigned num_rows, const unsigned num_cols);
+void nomp_calc_w_init(double *w_vec, const double alpha, double *orth_mtx,
+                      const unsigned col_index, const unsigned size);
+void nomp_calc_w(double *w_vec, const double alpha, double *orth_mtx,
+                 const double beta, const unsigned col_index,
+                 const unsigned size);
 
 // Parallelized functions of Lanczos routine implemented using Cuda.
 double cuda_vec_dot(double *d_a_vec, double *d_b_vec, const unsigned size);
