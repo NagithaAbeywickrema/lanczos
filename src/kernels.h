e7f26bebb4d3fd55002f1abc15fa16441095b6eb
NagithaAbeywickrema <nagitha98@gmail.com>
2023-06-07 18:07:12 +0530

    Add Cuda spmv

--- src/kernels.h
+++ src/kernels.h
@@ -14,8 +14,9 @@ void serial_mtx_col_copy(double *vec, double *mtx, const unsigned col_index,
                          const unsigned size);
 void serial_mtx_vec_mul(double *a_mtx, double *b_vec, double *out_vec,
                         const unsigned num_rows, const unsigned num_cols);
-void serial_spmv(int *a_row_ptrs, int *a_columns, double *a_vals, double *b_vec, double *out_vec,
-                        const unsigned num_rows, const unsigned num_cols);
+void serial_spmv(int *a_row_ptrs, int *a_columns, double *a_vals, double *b_vec,
+                 double *out_vec, const unsigned num_rows,
+                 const unsigned num_cols);
 void serial_calc_w_init(double *w_vec, const double alpha, double *orth_mtx,
                         const unsigned col_index, const unsigned size);
 void serial_calc_w(double *w_vec, const double alpha, double *orth_mtx,
@@ -31,8 +32,9 @@ void nomp_mtx_col_copy(double *vec, double *mtx, const unsigned col_index,
                        const unsigned size);
 void nomp_mtx_vec_mul(double *a_mtx, double *b_vec, double *out_vec,
                       const unsigned num_rows, const unsigned num_cols);
-void nomp_spmv(int *a_row_ptrs, int *a_columns, double *a_vals, double *b_vec, double *out_vec,
-                        const unsigned num_rows, const unsigned num_cols);
+void nomp_spmv(int *a_row_ptrs, int *a_columns, double *a_vals, double *b_vec,
+               double *out_vec, const unsigned num_rows,
+               const unsigned num_cols);
 void nomp_calc_w_init(double *w_vec, const double alpha, double *orth_mtx,
                       const unsigned col_index, const unsigned size);
 void nomp_calc_w(double *w_vec, const double alpha, double *orth_mtx,
@@ -48,6 +50,9 @@ void cuda_mtx_col_copy(double *d_vec, double *d_mtx, const unsigned col_index,
                        const unsigned size);
 void cuda_mtx_vec_mul(double *d_a_mtx, double *d_b_vec, double *d_out_vec,
                       const unsigned num_rows, const unsigned num_cols);
+void cuda_spmv(int *d_a_row_ptrs, int *d_a_columns, double *d_a_vals,
+               double *d_b_vec, double *d_out_vec, const unsigned num_rows,
+               const unsigned num_cols);
 void cuda_calc_w_init(double *d_w_vec, const double alpha, double *d_orth_mtx,
                       const unsigned col_index, const unsigned size);
 void cuda_calc_w(double *d_w_vec, const double alpha, double *d_orth_mtx,
--- src/lanczos-cuda.cu
+++ src/lanczos-cuda.cu
@@ -6,9 +6,10 @@
 #define MAX 10
 #define EPS 1e-12
 
-void lanczos_algo(double *d_lap, double *alpha, double *beta, double *d_w_vec,
-                  double *w_vec, double *d_orth_vec, double *orth_vec,
-                  double *d_orth_mtx, const unsigned m, const unsigned size) {
+void lanczos_algo(int *d_row_ptrs, int *d_columns, double *d_vals,
+                  double *alpha, double *beta, double *d_w_vec, double *w_vec,
+                  double *d_orth_vec, double *orth_vec, double *d_orth_mtx,
+                  const unsigned m, const unsigned size) {
   cudaMemcpy(d_w_vec, w_vec, (size) * sizeof(double), cudaMemcpyHostToDevice);
 
   for (unsigned i = 0; i < m; i++) {
@@ -27,7 +28,8 @@ void lanczos_algo(double *d_lap, double *alpha, double *beta, double *d_w_vec,
 
     cuda_mtx_col_copy(d_orth_vec, d_orth_mtx, i, size);
 
-    cuda_mtx_vec_mul(d_lap, d_orth_vec, d_w_vec, size, size);
+    // cuda_mtx_vec_mul(d_lap, d_orth_vec, d_w_vec, size, size);
+    cuda_spmv(d_row_ptrs, d_columns, d_vals, d_orth_vec, d_w_vec, size, size);
 
     alpha[i] = cuda_vec_dot(d_orth_vec, d_w_vec, size);
 
@@ -39,8 +41,9 @@ void lanczos_algo(double *d_lap, double *alpha, double *beta, double *d_w_vec,
   }
 }
 
-void lanczos(double *lap, const unsigned size, const unsigned m,
-             double *eigvals, double *eigvecs, int argc, char *argv[]) {
+void lanczos(int *row_ptrs, int *columns, double *vals, int val_count,
+             const unsigned size, const unsigned m, double *eigvals,
+             double *eigvecs, int argc, char *argv[]) {
   // Allocate host memory
   double *alpha = (double *)calloc(m, sizeof(double));
   double *beta = (double *)calloc(m, sizeof(double));
@@ -48,27 +51,34 @@ void lanczos(double *lap, const unsigned size, const unsigned m,
   double *w_vec = (double *)calloc(size, sizeof(double));
 
   // Device memory
-  double *d_lap, *d_orth_mtx, *d_orth_vec, *d_w_vec;
+  double *d_vals, *d_orth_mtx, *d_orth_vec, *d_w_vec;
+  int *d_row_ptrs, *d_columns;
 
   // Allocate device memory
-  cudaMalloc((void **)&d_lap, (size * size) * sizeof(double));
+  cudaMalloc((void **)&d_row_ptrs, (size + 1) * sizeof(int));
+  cudaMalloc((void **)&d_columns, (val_count) * sizeof(int));
+  cudaMalloc((void **)&d_vals, (val_count) * sizeof(double));
   cudaMalloc((void **)&d_orth_mtx, (size * m) * sizeof(double));
   cudaMalloc((void **)&d_orth_vec, (size) * sizeof(double));
   cudaMalloc((void **)&d_w_vec, (size) * sizeof(double));
 
   // H2D memory copy
-  cudaMemcpy(d_lap, lap, (size * size) * sizeof(double),
+  cudaMemcpy(d_row_ptrs, row_ptrs, (size + 1) * sizeof(int),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_columns, columns, (val_count) * sizeof(int),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_vals, vals, (val_count) * sizeof(double),
              cudaMemcpyHostToDevice);
 
   // Warm up runs
   for (unsigned k = 0; k < 10; k++)
-    lanczos_algo(d_lap, alpha, beta, d_w_vec, w_vec, d_orth_vec, orth_vec,
-                 d_orth_mtx, m, size);
+    lanczos_algo(d_row_ptrs, d_columns, d_vals, alpha, beta, d_w_vec, w_vec,
+                 d_orth_vec, orth_vec, d_orth_mtx, m, size);
 
   // Measure time
   clock_t t = clock();
-  lanczos_algo(d_lap, alpha, beta, d_w_vec, w_vec, d_orth_vec, orth_vec,
-               d_orth_mtx, m, size);
+  lanczos_algo(d_row_ptrs, d_columns, d_vals, alpha, beta, d_w_vec, w_vec,
+               d_orth_vec, orth_vec, d_orth_mtx, m, size);
   t = clock() - t;
 
   printf("size: %d, time: %e \n", size, (double)t / (CLOCKS_PER_SEC));
@@ -77,8 +87,8 @@ void lanczos(double *lap, const unsigned size, const unsigned m,
   print_eigen_vals(eigvals, size);
 
   // Free device memory
-  cudaFree(d_lap), cudaFree(d_orth_mtx), cudaFree(d_orth_vec),
-      cudaFree(d_w_vec);
+  cudaFree(d_row_ptrs), cudaFree(d_columns), cudaFree(d_vals),
+      cudaFree(d_orth_mtx), cudaFree(d_orth_vec), cudaFree(d_w_vec);
 
   // Free host memory
   free(alpha), free(beta), free(orth_vec), free(w_vec);
